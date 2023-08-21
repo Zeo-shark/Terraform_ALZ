4de556a7cd2e155afe8bd1273944d885bbf6298d
Zeo-shark <sauravberaiit@gmail.com>
2023-08-21 22:35:15 +0530

    minor fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,9 +14,7 @@ terraform {
 }
 
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "3.70.0"
+  skip_provider_registration = true
   features {}
 }
 
