d7e1e3a2ad29e6708bff4d7e5696dc1ea861c10c
Zeo-shark <sauravberaiit@gmail.com>
2023-08-23 00:08:03 +0530

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-# We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used.
 
 terraform {
@@ -15,7 +14,6 @@ provider "azurerm" {
   features {}
 }
 
-
 # You can use the azurerm_client_config data resource to dynamically
 # extract connection settings from the provider configuration.
 
@@ -26,7 +24,7 @@ data "azurerm_client_config" "core" {}
 
 module "enterprise_scale" {
   source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "=4.2.0" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
+  version = "4.2.0" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
 
   default_location = "eastus"
 
@@ -36,7 +34,7 @@ module "enterprise_scale" {
     azurerm.management   = azurerm
   }
 
-  root_parent_id = "dc165e1d-5d8f-405b-8521-0b1dc3a1aee3"
+  root_parent_id = data.azurerm_client_config.core.tenant_id
   root_id        = "myorg"
   root_name      = "My Organization"
 }
