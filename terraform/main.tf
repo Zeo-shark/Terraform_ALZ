7b4478bce2b2b85c657f028d1f3176aaf2112ba6
Zeo-shark <sauravberaiit@gmail.com>
2023-08-23 13:59:25 +0530

    trying with basic terraform template

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,8 @@
-# Azure Provider source and version being used.
-
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.54.0"
+      source = "hashicorp/azurerm"
+      version = "3.70.0"
     }
   }
 }
@@ -12,29 +10,11 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+  
 }
 
-# You can use the azurerm_client_config data resource to dynamically
-# extract connection settings from the provider configuration.
-
-data "azurerm_client_config" "core" {}
-
-# Call the caf-enterprise-scale module directly from the Terraform Registry
-# pinning to the latest version
-
-module "enterprise_scale" {
-  source  = "Azure/caf-enterprise-scale/azurerm"
-  version = "4.2.0" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
-
-  default_location = "eastus"
-
-  providers = {
-    azurerm              = azurerm
-    azurerm.connectivity = azurerm
-    azurerm.management   = azurerm
-  }
-
-  root_parent_id = data.azurerm_client_config.core.tenant_id
-  root_id        = "myorg"
-  root_name      = "My Organization"
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = "terraform-rg"
+  location = "West Europe"
 }
+
